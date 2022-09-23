61cb9ff292ee6d69a7b31c4abbeb38becac338b6
Paul Thomas <paulthomas2107@gmail.com>
2022-09-23 14:39:31 +0100

    More

--- app.py
+++ app.py
@@ -1,2 +1,20 @@
-from flask import Flask, render_template,  request, flash
+from flask import Flask, render_template,  request, flash, session
+
+app = Flask(__name__)
+app.secret_key = "super secret key....."
+
+
+@app.route("/greet", methods=["POST", "GET"])
+def greet():
+    flash("Hi " + str(request.form['name_input']) + ", " + "great to see you !")
+    return render_template("index.html")
+
+
+@app.route("/hello")
+def index():
+    flash("What is your name ?")
+    return render_template("index.html")
+
+
+
 
